2df960cf49539e3404f130f8b32a2576f16cba02
Zuzanna <zuzanna.lech77@gmail.com>
2025-12-17 22:16:25 +0100

    Fix: add wildcard to ALLOWED_HOSTS

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -70,7 +70,7 @@ if IS_HEROKU_APP:
     # https://docs.djangoproject.com/en/6.0/ref/middleware/#http-strict-transport-security
     SECURE_SSL_REDIRECT = True
 else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0", "[::]"]
+    ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
