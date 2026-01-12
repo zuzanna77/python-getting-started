40f9914634a2d477f6717d784e4465733f653489
Zuzanna <zuzanna.lech77@gmail.com>
2026-01-12 12:44:08 +0100

    Add build script and automated migrations

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -140,12 +140,12 @@ DATABASES = {
         conn_health_checks=True,
     )
 }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-} 
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.sqlite3",
+   #     "NAME": BASE_DIR / "db.sqlite3",
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
